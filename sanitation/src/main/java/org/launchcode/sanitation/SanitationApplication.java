5052dc8781d129dfba51c937bf15852a31908382
Koomi <koomiadeh@gmail.com>
2018-08-05 09:44:18 -0500

    the beginning

---
+++ sanitation/src/main/java/org/launchcode/sanitation/SanitationApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.sanitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanitationApplication.class, args);
+	}
+}
